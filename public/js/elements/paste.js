067a0584500e1fb435a28c45cbb88d62de292e9a
Jérémy Lal <kapouer@melix.org>
2021-02-10 23:54:05 +0100

    edit-paste: show url on hover

--- public/js/elements/paste.js
+++ public/js/elements/paste.js
@@ -2,38 +2,63 @@ import req from "../req.js";
 
 export default class EditPaste extends HTMLFormElement {
 	#input
+	#anchor
 	constructor() {
 		super();
 		this.setAttribute('is', 'edit-paste');
 	}
 	connectedCallback() {
 		this.addEventListener('click', this);
+		this.addEventListener('mouseenter', this);
+		this.addEventListener('mouseleave', this);
 		this.#input = this.querySelector('input');
+		this.#anchor = document.createElement("a");
 	}
 	disconnectedCallback() {
 		this.removeEventListener('click', this);
+		this.removeEventListener('mouseenter', this);
+		this.removeEventListener('mouseleave', this);
+		this.#input = null;
+		this.#anchor = null;
 	}
-	handleEvent(e) {
-		navigator.clipboard.readText().then(txt => {
-			this.add(txt);
-		});
+	validateUrl(url) {
+		if (!url || !/^https?:\/\//.test(url)) {
+			return false;
+		}
+		const p = this.#anchor;
+		p.href = url;
+		return !p.username && !p.password
+			&& /\.[^0-9.]/.test(p.hostname)
+			&& !/(\s|^\.|\.$)/.test(p.hostname);
 	}
-	async add(str) {
+	async paste() {
+		try {
+			const txt = await navigator.clipboard.readText();
+			if (!this.validateUrl(txt) || txt.length > 2048) return;
+			this.#input.value = txt.trim();
+		} catch {
+			// do nothing
+		}
+	}
+	async handleEvent(e) {
+		const node = this.#input;
+		if (node.disabled) return;
+		if (e.type == "mouseenter") {
+			this.paste();
+		} else if (e.type == "mouseleave") {
+			node.value = "";
+		} else if (e.type == "click") {
+			e.preventDefault();
+			node.blur();
+			if (node.value.length == 0) await this.paste();
+			if (node.value.length > 0) this.submit();
+		}
+	}
+	async submit() {
 		this.classList.add('loading');
 		this.#input.disabled = true;
-		str = str.trim();
-		this.#input.value = str;
-		let asset;
 		try {
-			if (str.startsWith('<')) {
-				// TODO implement html snippets
-			} else {
-				if (!/^[a-zA-Z]+:\/\//.test(str)) str = 'https://' + str;
-				const anchor = document.createElement("a");
-				anchor.setAttribute("href", str);
-				const url = anchor.href;
-				asset = await req("./assets", "post", { url });
-			}
+			await req("./assets", "post", { url: this.#input.value });
 			this.#input.value = "";
 		} catch (err) {
 			this.classList.add("error");
@@ -46,6 +71,5 @@ export default class EditPaste extends HTMLFormElement {
 		}
 		this.#input.disabled = false;
 		this.classList.remove('loading');
-		return asset;
 	}
 }
