24526d0c7c46336d1c45fad4668e50d9ddf55f05
Jérémy Lal <kapouer@melix.org>
2021-02-10 22:39:08 +0100

    Remove luxon on client, part two

--- public/js/live.js
+++ public/js/live.js
@@ -1,6 +1,15 @@
 import { Matchdom } from "../modules/matchdom";
 import "./array-like.js";
 
+const months = [
+	'janvier', 'février', 'mars', 'avril', 'mai', 'juin',
+	'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'
+];
+
+function getTimeStr(date) {
+	return `${date.getHours()}:${date.getMinutes()}`;
+}
+
 const types = {
 	date(ctx, val) {
 		if (val == null) return val;
@@ -34,9 +43,9 @@ const filters = {
 	},
 	date: ['date?now', 'string', (ctx, date, fmt) => {
 		if (fmt == "iso") {
-			return date.toJSDate().toISOString();
+			return date.toISOString();
 		} else if (fmt == "cal") {
-			return date.toFormat("DDD 'à' T");
+			return `le ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()} à ${getTimeStr(date)}`;
 		} else if (fmt == "rel") {
 			let sameDay = false;
 			const live = ctx.scope.live;
@@ -44,7 +53,7 @@ const filters = {
 				const ref = new Date(live.rooms.page);
 				sameDay = Math.abs((date.getTime() - ref.getTime())) < 1000 * 3600 * 24;
 			}
-			const time = `${date.getHours()}:${date.getMinutes()}`;
+			const time = getTimeStr(date);
 			if (sameDay) {
 				return time;
 			} else {
