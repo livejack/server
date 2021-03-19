398ac2f10fb02abd39eac943aa1c01feaa883acb
Jérémy Lal <kapouer@melix.org>
2021-03-19 15:43:13 +0100

    editor: improve paste of html snippet/text detection

--- public/js/editor/index.js
+++ public/js/editor/index.js
@@ -76,35 +76,76 @@ export class Editor extends EditorView {
 			nodeViews,
 			domParser: parser,
 			transformPastedHTML: (str) => {
-				let frag = parseHTML(str);
-				if (frag.nodeType == Node.DOCUMENT_FRAGMENT_NODE) {
-					let text = frag.textContent;
-					if (text.startsWith('<') && text.endsWith('>')) {
-						return this.props.transformPastedHTML(text);
+				if (str.startsWith('<') && str.endsWith('>')) {
+					let frag = parseHTML(str);
+					if (frag.nodeType == Node.DOCUMENT_FRAGMENT_NODE) {
+						if (frag.childNodes.length == 2 && frag.firstChild.nodeName == "META" && frag.lastChild.nodeName == "SPAN") {
+							frag = parseHTML(frag.lastChild.textContent);
+						}
 					}
+					frag = this.convertSnippets(frag);
+					const div = frag.ownerDocument.createElement('div');
+					div.appendChild(frag);
+					return div.innerHTML;
 				}
-				if (frag.dataset && frag.dataset.url) return this.convertAsset(frag).outerHTML;
-				else return this.convertSnippet(frag) || str;
+				return str;
+			},
+			clipboardTextParser: (str, $pos) => {
+				if (str.startsWith('<') && str.endsWith('>')) {
+					const frag = this.convertSnippets(parseHTML(str));
+					return this.props.domParser.parseSlice(frag, {
+						context: $pos
+					});
+				}
 			}
 		});
 		this.#content = nodes.doc.content.replace('*', '');
 		this.#parser = parser;
 		this.#serializer = DOMSerializer.fromSchema(schema);
 	}
-	convertSnippet(frag) {
-		const data = { html: "" };
-		for (let child of frag.childNodes) {
-			if (child.nodeType == Node.TEXT_NODE && /\s*/.test(child.nodeValue)) continue;
-			if (child.nodeType != Node.ELEMENT_NODE) return;
-			if (child.nodeName == "SCRIPT" && child.src) data.script = child.src;
-			else if (child.nodeName == "IFRAME" && child.src) data.url = child.src;
-			else data.html += child.outerHTML;
+	convertSnippets(frag) {
+		if (frag.nodeType == Node.ELEMENT_NODE) {
+			// use fragment
+			frag = frag.parentNode;
 		}
-		if (!data.script && !data.url) return;
-		if (data.html.trim() == "") delete data.html;
-		const dom = document.createElement('live-asset');
-		Object.assign(dom.dataset, data);
-		return dom.outerHTML;
+		const data = { html: "" };
+		const copy = document.createDocumentFragment();
+
+		for (let child of frag.childNodes) {
+			if (child.nodeType == Node.TEXT_NODE && child.nodeValue.trim().length == 0) {
+				continue;
+			} else if (child.nodeType != Node.ELEMENT_NODE) {
+				copy.appendChild(child.cloneNode(true));
+				continue;
+			}
+			if (child.nodeName == "SCRIPT") {
+				if (data.node) {
+					if (child.src) {
+						data.script = child.src;
+						// this finishes the sequence
+						const dom = document.createElement('live-asset');
+						if (data.node) data.html = data.node.outerHTML + data.html;
+						Object.assign(dom.dataset, data);
+						data.node = null;
+						data.html = "";
+						data.script = "";
+						copy.appendChild(dom);
+					} else {
+						data.html += child.outerHTML;
+					}
+				} else {
+					// do not copy lone script
+				}
+			} else {
+				if (data.node) {
+					// previous node was an element, let it be parsed
+					copy.appendChild(data.node.cloneNode(true));
+				}
+				data.node = child;
+			}
+		}
+		if (data.node) copy.appendChild(data.node.cloneNode(true));
+		return copy;
 	}
 
 	convertAsset(dom) {
