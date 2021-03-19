d0a0243403c0e85a0a17d11a0131f1ca5887e8aa
Jérémy Lal <kapouer@melix.org>
2021-03-19 16:29:50 +0100

    read: need to handle errors here

--- routes/read.js
+++ routes/read.js
@@ -3,13 +3,15 @@ const prerender = require('../lib/prerender');
 
 exports.GET = async function(req, res, next) {
 	const { domain, key } = req.params;
-	if (!req.domain) throw new HttpError.BadRequest("No domain");
-
-	await Models.Page.have({
-		domain, key,
-		view: req.domain.view
-	});
-
-	prerender(`live-read`)(req, res, next);
+	try {
+		if (!req.domain) throw new HttpError.BadRequest("No domain");
+		await Models.Page.have({
+			domain, key,
+			view: req.domain.view
+		});
+		prerender(`live-read`)(req, res, next);
+	} catch (err) {
+		next(err);
+	}
 };
 
