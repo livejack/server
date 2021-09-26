3f90affdbb3fa1b3cc931d715394ea0d243434d8
Jérémy Lal <kapouer@melix.org>
2021-09-26 22:06:11 +0200

    Lint

--- migrations/02-dates.js
+++ migrations/02-dates.js
@@ -1,19 +1,19 @@
 exports.up = async function (knex) {
-	await knex.schema.table('pages', function (table) {
+	await knex.schema.table('pages', (table) => {
 		table.timestamps(true, true);
 	});
 	await knex('pages').update('created_at', knex.ref('date'));
 	await knex('pages').update('updated_at', knex.ref('update')).whereNotNull('update');
-	await knex.schema.table('pages', function (table) {
+	await knex.schema.table('pages', (table) => {
 		table.dropColumn('date');
 		table.dropColumn('update');
 	});
-	await knex.schema.table('pages', function (table) {
+	await knex.schema.table('pages', (table) => {
 		table.renameColumn('start', 'start_noz');
 		table.renameColumn('stop', 'stop_noz');
 	});
 
-	await knex.schema.table('pages', function (table) {
+	await knex.schema.table('pages', (table) => {
 		table.timestamp('start', { useTz: true });
 		table.timestamp('stop', { useTz: true });
 	});
@@ -21,35 +21,35 @@ exports.up = async function (knex) {
 	await knex('pages').update('start', knex.ref('start_noz'));
 	await knex('pages').update('stop', knex.ref('stop_noz'));
 
-	await knex.schema.table('pages', function (table) {
+	await knex.schema.table('pages', (table) => {
 		table.dropColumn('start_noz');
 		table.dropColumn('stop_noz');
 	});
 
-	await knex.schema.table('messages', function (table) {
+	await knex.schema.table('messages', (table) => {
 		table.timestamps(true, true);
 	});
 	await knex('messages').update('created_at', knex.ref('date'));
 	await knex('messages').update('updated_at', knex.ref('update')).whereNotNull('update');
 
-	await knex.schema.table('messages', function (table) {
+	await knex.schema.table('messages', (table) => {
 		table.renameColumn('date', 'date_noz');
 	});
 	await knex('messages').update('date', knex.ref('date_noz'));
-	await knex.schema.table('messages', function (table) {
+	await knex.schema.table('messages', (table) => {
 		table.dropColumn('date_noz');
 	});
 
-	await knex.schema.table('messages', function (table) {
+	await knex.schema.table('messages', (table) => {
 		table.dropColumn('update');
 	});
 };
 
 exports.down = async function (knex) {
-	await knex.schema.table('pages', function (table) {
+	await knex.schema.table('pages', (table) => {
 		table.dropTimestamps();
 	});
-	await knex.schema.table('messages', function (table) {
+	await knex.schema.table('messages', (table) => {
 		table.dropTimestamps();
 	});
 };
--- migrations/03-no_id.js
+++ migrations/03-no_id.js
@@ -1,21 +1,21 @@
 exports.up = async function (knex) {
 	if (await knex.schema.hasColumn('pages', '_id')) {
-		await knex.schema.table('pages', function (table) {
+		await knex.schema.table('pages', (table) => {
 			table.dropColumn('_id');
 		});
 	}
 	if (await knex.schema.hasColumn('messages', '_id')) {
-		await knex.schema.table('messages', function (table) {
+		await knex.schema.table('messages', (table) => {
 			table.dropColumn('_id');
 		});
 	}
 };
 
 exports.down = async function (knex) {
-	await knex.schema.table('pages', function (table) {
+	await knex.schema.table('pages', (table) => {
 		table.string('_id');
 	});
-	await knex.schema.table('messages', function (table) {
+	await knex.schema.table('messages', (table) => {
 		table.string('_id');
 	});
 };
