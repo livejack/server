89085ea97d4f8b66a69d6a51fa563d9d456737ee
Jérémy Lal <kapouer@melix.org>
2021-10-26 11:32:49 +0200

    edit-paste: buttons

--- public/js/elements/edit-paste.js
+++ public/js/elements/edit-paste.js
@@ -9,15 +9,21 @@ export default class EditPaste extends HTMLFormElement {
 	}
 	connectedCallback() {
 		this.addEventListener('submit', this);
-		this.addEventListener('paste', this);
+		this.addEventListener('reset', this);
+		this.addEventListener('input', this);
+		this.addEventListener('change', this);
 		this.addEventListener('focusin', this);
 		this.#input = this.querySelector('input');
+		this.#input.addEventListener('focus', this);
 		this.#anchor = document.createElement("a");
 	}
 	disconnectedCallback() {
 		this.removeEventListener('submit', this);
-		this.removeEventListener('paste', this);
+		this.removeEventListener('reset', this);
+		this.removeEventListener('input', this);
+		this.removeEventListener('change', this);
 		this.removeEventListener('focusin', this);
+		this.#input.removeEventListener('focus', this);
 		this.#input = null;
 		this.#anchor = null;
 	}
@@ -31,25 +37,25 @@ export default class EditPaste extends HTMLFormElement {
 			&& /\.[^0-9.]/.test(p.hostname)
 			&& !/(\s|^\.|\.$)/.test(p.hostname);
 	}
-	async handleEvent(e) {
-		if(e.type == "submit") {
+	handleEvent(e) {
+		if (e.type == "submit") {
 			e.preventDefault();
 			this.#input.blur();
-			try {
-				await this.create(this.#input.value);
-			} catch (e) {
+			this.create(this.#input.value).catch((err) => {
 				this.classList.add('error');
-			}
+			});
 		} else if (e.type == "focusin") {
-			this.classList.remove('error');
-			this.#input.value = "";
-		} else if (e.type == "paste") {
-			this.#input.blur();
-			try {
-				await this.create(e.clipboardData.getData('text'));
-			} catch (e) {
-				this.classList.add('error');
+			if (this.classList.contains('error')) {
+				this.classList.remove('error');
+				this.#input.value = "";
+				this.update();
 			}
+		} else if (e.type == "input" || e.type == "change" || e.type == "reset") {
+			window.requestAnimationFrame(() => {
+				this.update();
+			});
+		} else if (e.type == "focus") {
+			this.#input.select();
 		}
 	}
 	async create(url) {
@@ -64,14 +70,24 @@ export default class EditPaste extends HTMLFormElement {
 		this.classList.add('loading');
 		this.#input.disabled = true;
 		let item;
+		const url = this.#input.value;
 		try {
-			item = await req("./assets", "post", { url: this.#input.value });
+			item = await req("./assets", "post", { url });
 			this.#input.value = "";
 		} catch (err) {
 			this.classList.add("error");
 		}
 		this.#input.disabled = false;
 		this.classList.remove('loading');
+		this.update();
+		if (item) this.change(item.url);
 		return item;
+	}
+	update() {
+		this.querySelector('.buttons')
+			.classList.toggle('hide', !this.#input.value);
+	}
+	change(url) {
+		// do nothing
 	}
 }
