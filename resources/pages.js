5390d5185f3299c7c58b002373bda45975ff4689
Jérémy Lal <kapouer@melix.org>
2021-11-16 11:33:54 +0100

    Use req.params.view

--- resources/pages.js
+++ resources/pages.js
@@ -2,14 +2,13 @@ const {Models} = require('objection');
 const {Page} = Models;
 
 exports.GET = async (req, res, next) => {
-	const {domain} = req.params;
+	const { domain } = req.params;
 	const pages = await Page.query().where({ domain }).orderBy('updated_at', 'desc').limit(20);
 	res.json(pages);
 };
 
 exports.PUT = async (req, res, next) => {
-	const {domain, key} = req.params;
-	await Page.have({domain, key, view: req.domain.view});
+	const {domain, key, view} = req.params;
+	await Page.have({ domain, key, view });
 	res.sendStatus(204);
 };
-
--- routes/read.js
+++ routes/read.js
@@ -1,16 +1,12 @@
 const {Models} = require('objection');
 
 exports.GET = async function(req, res, next) {
-	const { domain, key } = req.params;
+	const { domain, key, view } = req.params;
 	try {
 		if (!req.domain) throw new HttpError.BadRequest("No domain");
-		await Models.Page.have({
-			domain, key,
-			view: req.domain.view
-		});
+		await Models.Page.have({ domain, key, view });
 		res.prerender('read.html');
 	} catch (err) {
 		next(err);
 	}
 };
-
