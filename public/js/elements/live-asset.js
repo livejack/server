762939041311aab960095b52272bea0406c189f7
Jérémy Lal <kapouer@melix.org>
2021-12-14 14:57:37 +0100

    Do not reinstall global scripts

--- public/js/elements/live-asset.js
+++ public/js/elements/live-asset.js
@@ -78,8 +78,11 @@ export class LiveAsset extends HTMLElement {
 			this.querySelector('img,iframe').src = url;
 		}
 		if (script) {
-			this.insertAdjacentHTML('beforeEnd', '<script defer=""></script>');
-			this.lastElementChild.setAttribute('src', script);
+			if (doc.head.querySelector(`script[src="${script}"]`) == null) {
+				// do not reinstall globally installed scripts
+				this.insertAdjacentHTML('beforeEnd', '<script defer=""></script>');
+				this.lastElementChild.setAttribute('src', script);
+			}
 		}
 		for (const node of this.querySelectorAll('script')) {
 			const copy = doc.createElement('script');
