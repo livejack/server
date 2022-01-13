0863b37eab5f17784573a4a829d878bb81acf171
Jérémy Lal <kapouer@melix.org>
2022-01-13 18:31:39 +0100

    lives.json: remove "when", keep "start"

--- routes/lives.js
+++ routes/lives.js
@@ -1,4 +1,4 @@
-const {Models} = require('objection');
+const { Models: { Page } } = require('objection');
 
 exports.GET = async (req, res, next) => {
 	let domains = req.query.domains || [];
@@ -7,22 +7,21 @@ exports.GET = async (req, res, next) => {
 	});
 	const now = new Date();
 
-	const pages = await Models.Page.query()
+	const pages = await Page.query()
 		.select('domain', 'key', 'updated_at AS last', 'title', 'backtrack', 'start', 'stop')
 		.orderBy('updated_at', 'desc')
 		.whereNotNull('title')
 		.whereNotNull('backtrack')
-		.where('start', '<', now)
+		.whereNotNull('start')
+		.where('start', '<', now.toISOString())
 		.where(function() {
-			this.whereNull('stop').orWhere('stop', '>', now);
+			this.whereNull('stop').orWhere('stop', '>', now.toISOString());
 		})
 		.whereRaw('length(title) > 0');
 	if (domains.length) pages.whereIn('domain', domains);
 	const items = pages.map(page => {
 		const item = Object.assign({}, page);
-		delete item.start;
 		delete item.stop;
-		item.when = page.when;
 		return item;
 	});
 
