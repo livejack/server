aeca62634ad44bfcf5999540eddcd019fdf5cb2f
Jérémy Lal <kapouer@melix.org>
2022-02-25 12:18:06 +0100

    Support tokens

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const { BaseModel } = require('objection');
+const { BaseModel, transaction } = require('objection');
 const DiffList = require('diff-list');
 
 class User extends BaseModel {
@@ -9,33 +9,49 @@ class User extends BaseModel {
 	static get jsonSchema() {
 		return {
 			type: 'object',
-			required: ['domain', 'token'],
+			required: ['domain', 'tokens'],
 			properties: {
 				domain: {
 					type: 'string'
 				},
-				token: {
-					type: 'string'
+				tokens: {
+					type: 'array',
+					items: {
+						type: 'string'
+					}
 				}
 			}
 		};
 	}
 
 	static async populate(nusers) {
-		const users = await this.query();
-		const diff = DiffList(users, nusers, {
-			key: 'domain',
-			equal: function(a, b) {
-				return a.token === b.token;
+		const trx = await transaction.start(this);
+		try {
+			const users = await this.query(trx);
+			const diff = DiffList(users, nusers, {
+				key: 'domain',
+				equal: function (a, b) {
+					return (a.tokens || []).join(',') == (b.tokens || []).join(',');
+				}
+			});
+			for (const user of diff.put) {
+				await this.query(trx).findOne({
+					domain: user.domain
+				}).throwIfNotFound().patch(user);
 			}
-		});
-		for (const user of diff.put) {
-			await this.query().findOne({
-				domain: user.domain
-			}).throwIfNotFound().patch(user);
+			if (diff.post.length) {
+				await this.query(trx).insert(diff.post);
+			}
+			if (diff.del.length) {
+				await this.query(trx)
+					.whereIn('domain', diff.del.map((item) => item.domain))
+					.delete();
+			}
+			await trx.commit();
+		} catch (err) {
+			await trx.rollback();
+			throw err;
 		}
-		await this.query().insert(diff.post);
-		await this.query().whereIn('domain', diff.del.map((item) => item.domain)).delete();
 	}
 }
 
